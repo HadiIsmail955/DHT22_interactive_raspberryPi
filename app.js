580607f6784ffe647b816eee7ce413c50431c574
HadiIsmail955 <hadiismail955@gmail.com>
2024-06-17 20:37:47 +0100

    updated

--- app.js
+++ app.js
@@ -13,28 +13,27 @@ app.use(parser.urlencoded());
 app.use("/", router);
 //app.use("/auth");
 
-// const Generator = require("./class/generator.js");
-// const start = async () => {
-//   let generators = [];
-//   const generateController = require("./controllers/generatorController.js");
-//   const generatorFound = await generateController.getAllGenerators();
-//   // console.log(generatorFound)
-//   generatorFound?.forEach((generator) => {
-//     generators.push(
-//       new Generator(
-//         generator.id,
-//         generator.generatorName,
-//         generator.generateCooling,
-//         generator.generateHeating,
-//         generator.coolingPin,
-//         generator.heatingPin
-//       )
-//     );
-//   });
-
-//   // */
-// };
-// start();
+ const Generator = require("./class/generator.js");
+ const start = async () => {
+   let generators = [];
+   const generateController = require("./controllers/generatorController.js");
+   const generatorFound = await generateController.getAllGenerators();
+   // console.log(generatorFound)
+   generatorFound?.forEach((generator) => {
+     generators.push(
+       new Generator(
+         generator.id,
+         generator.generatorName,
+         generator.generateCooling,
+         generator.generateHeating,
+         generator.coolingPin,
+         generator.heatingPin
+       )
+     );
+   });
+   // */
+ };
+ start();
 app.listen(process.env.PORT || 5001, () => {
   try {
     console.log("connnected at port " + process.env.PORT);
